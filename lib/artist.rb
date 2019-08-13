dc78ad91fad3d68a5904589f9f30e85c335ed6b0
Renee Sims <info@reneesoul.com>
2019-08-13 22:30:04 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,3 +1,20 @@
 def Artist
+  attr_accessor :name
+  
+  def initialize (name)
+    @name = name
+    @@all << self
+  end
+  
+  def name=(name)
+    @name = name
+  end
+  
+  
+  def sefl.all
+    @@all
+  end
+  
+end
   
   
