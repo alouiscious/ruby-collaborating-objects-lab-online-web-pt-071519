68915605d249f835b63224dacb0b285c16a731f4
Renee Sims <info@reneesoul.com>
2019-08-13 23:04:32 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,28 +1,30 @@
 def Artist
+  
   attr_accessor :name
   
+  def sefl.all
+    @@all
+    
+  end 
   
   def initialize (name)
     @name = name
     @@all << self
   end
   
+  
   def name=(name)
     @name = name
   end
   
   
-  def sefl.all
-    @@all
-    
-  end
+ 
   
   def songs
     Song.all.select do |song|
       song.artist == self
     end
-    @songs
-    
+
   end
   
 end
